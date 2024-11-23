3ce2920d89a0d3d9983be34008c248c846938930
nobo kumar <nobokumar227@gmail.com>
2024-11-23 21:28:32 +0600

    add toggle button

--- script.js
+++ script.js
@@ -30,5 +30,49 @@ form.addEventListener('submit', (event)=>{
 });
 
 
+const myLibrary = [
+    {title: "Atomic Habits", author: "James Clear", pages: 320, status: "read"},
+    {title: "Rich Dad Poor Dad", author: "Robert T.Kiyosaki", pages: 220, status: "read"},
+    {title: "The Psychology of Money", author: "Morgan Housel", pages: 256, status: "not read"},
+    {title: "Ikigai", author: "Francesc Miralles", pages: 305, status: "read"},
+    {title: "Deep Work", author: "Cal Newport", pages: 208, status: "not read"}
+];
 
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+};
 
+//add a book to array
+function addBookToLibrary(book) {
+  myLibrary.push(book)
+  bookCard();
+};
+
+function bookCard(){
+    const bookContainer = document.getElementById('book-container');
+    bookContainer.innerHTML = ``;
+
+            myLibrary.forEach((book, index)=>{
+                const card = document.createElement('div');
+                card.classList.add('card');
+                
+                card.innerHTML = `
+                <button class="close-button" aria-label="Close">x</button>
+                <h3>Title: ${book.title}</h3>
+                <p>Author: ${book.author}</p>
+                <p>Pages: ${book.pages}</p>
+                <button class = "status-button">${book.status}</button>`
+
+                bookContainer.appendChild(card);
+
+                const statusButton = card.querySelector('.status-button');
+                statusButton.addEventListener('click', ()=>{
+                    book.status = book.status === "read"? "not read":"read";
+                    statusButton.textContent = book.status;
+                });
+            });
+}
+bookCard();
